394b6394426ef39d0b7be114b9c126f88e486887
Somil Verma <somilverma11@gmail.com>
2023-10-30 18:52:47 +0530

    Inheritance in Ruby

---
+++ Ruby/Practice_Programs/Classes/inheritance.rb
@@ -0,0 +1,24 @@
+class Animal
+    def walk
+        puts "Animal can walk"
+    end
+
+    def eat
+        puts "Animal can eat"
+    end
+
+end
+
+class Dog < Animal
+    def voice
+        puts "Dog can bark"
+    end
+
+end
+
+d=Dog.new
+d.walk
+d.eat
+d.voice
+
+
