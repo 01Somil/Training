1f88af873b00017f8b67e92ab4fd8faf65fd8bce
01Somil <somilverma11@gmail.com>
2023-11-08 17:28:26 +0530

    Add foreign key to enrolled subjects from students table

--- Rails/classroom_management/db/migrate/20231108112546_add_foreign_key_to_enrolled_subjects.rb
+++ Rails/classroom_management/db/migrate/20231108112546_add_foreign_key_to_enrolled_subjects.rb
@@ -1,6 +1,10 @@
 class AddForeignKeyToEnrolledSubjects < ActiveRecord::Migration[7.1]
   def change
-    add_foreign_key :enrolled_subjects, :subjects 
-    add_foreign_key :enrolled_subjects, :students, column: :roll_no
+    add_column :enrolled_subjects, :student_id, :integer
+    change_table :students do |t|
+      t.remove :roll_no
+      t.integer :roll_no,primary_key: true
+    end
+    add_foreign_key :enrolled_subjects, :students, column: :student_id, primary_key: :roll_no
   end
 end
