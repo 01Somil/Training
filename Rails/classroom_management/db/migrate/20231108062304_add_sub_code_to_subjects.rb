4536194927e5e8bc3ba86d4ab10f2040e95dc4fe
01Somil <somilverma11@gmail.com>
2023-11-08 12:09:53 +0530

    Adds another column sub_number to subjects table

--- Rails/classroom_management/db/migrate/20231108062304_add_sub_code_to_subjects.rb
+++ Rails/classroom_management/db/migrate/20231108062304_add_sub_code_to_subjects.rb
@@ -1,6 +1,6 @@
 class AddSubCodeToSubjects < ActiveRecord::Migration[7.1]
   def change
     add_column :subjects, :sub_code, :string
-    add_index :subjects, :sub_code
+    add_column :subjects, :sub_number, :integer
   end
 end
