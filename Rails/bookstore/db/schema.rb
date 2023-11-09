fcc7fb029db70cac042493b3fc696ce404c854e2
01Somil <somilverma11@gmail.com>
2023-11-09 15:48:31 +0530

    Migrate the up migrations into database

--- Rails/bookstore/db/schema.rb
+++ Rails/bookstore/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_09_093938) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_09_101513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_09_093938) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.integer "year_published"
-    t.boolean "out_of_stock"
+    t.boolean "out_of_stock", default: true
     t.integer "quantity"
     t.decimal "price", precision: 10, scale: 2
     t.bigint "author_id", null: false
@@ -91,7 +91,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_09_093938) do
   end
 
   create_table "sellers", force: :cascade do |t|
-    t.string "gstn"
+    t.text "gstn", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "country_id", null: false
