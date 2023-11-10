194d5d42445a063f5ef127dc797dddcd49be3c15
01Somil <somilverma11@gmail.com>
2023-11-10 13:37:08 +0530

    Migrate up migrations into database

--- Rails/bookstore/db/schema.rb
+++ Rails/bookstore/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_09_120658) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_10_060014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_09_120658) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.string "email_confirmation"
   end
 
   create_table "orders", force: :cascade do |t|
