2431d04ff0226e0216ff391c886554a40ad7adcc
01Somil <somilverma11@gmail.com>
2023-11-10 12:46:26 +0530

    Inclusion Validation on name to suppliers table

--- Rails/bookstore/app/models/supplier.rb
+++ Rails/bookstore/app/models/supplier.rb
@@ -1,3 +1,4 @@
 class Supplier < ApplicationRecord
     validates :name, format: {with: /\A[a-zA-Z]+\z/, message: "only allows letters"}
+    validates :name, inclusion: {in: %w(John Paul David), message: "%{value} is not a valid name"}
 end
