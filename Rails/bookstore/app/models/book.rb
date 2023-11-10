f47afa49dc8133f9aa331d6fa5a453a543378585
01Somil <somilverma11@gmail.com>
2023-11-10 13:36:33 +0530

    Adds Validates_associated on author_id,supplier_id to books table

--- Rails/bookstore/app/models/book.rb
+++ Rails/bookstore/app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ApplicationRecord
   belongs_to :author
   belongs_to :supplier
   validates :year_published, comparison: {greater_than: 2015}
+  validates_associated :authors
+  validates_associated :suppliers
 end
