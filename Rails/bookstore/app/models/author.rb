83ce87e76177548b5b4825666ff13a6d5252dd22
01Somil <somilverma11@gmail.com>
2023-11-10 11:45:16 +0530

    Presence validation added to authors table

--- Rails/bookstore/app/models/author.rb
+++ Rails/bookstore/app/models/author.rb
@@ -1,3 +1,6 @@
 class Author < ApplicationRecord
-    validates :first_name, presence: true
+    validates :first_name, acceptance: true
+    validates :last_name, acceptance: true
+    validates :title, acceptance: true
+    #validates :terms_of_service, acceptance: true
 end
