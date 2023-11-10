cefe533c3324ac966e8a1b47df88ca8d3754a452
01Somil <somilverma11@gmail.com>
2023-11-10 13:21:22 +0530

    Absence validation on title in customers table

--- Rails/bookstore/app/models/customer.rb
+++ Rails/bookstore/app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
     validates :email, confirmation: {case_sensitive: false}
     validates :email_confirmation, presence: true
+    validates :title, absence: true
 end
