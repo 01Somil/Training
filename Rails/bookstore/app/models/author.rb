3123cdf88ec55c15c6bb0106eaaf8c944b0435ea
01Somil <somilverma11@gmail.com>
2023-11-10 12:56:07 +0530

    Adds Length validations to authors table

--- Rails/bookstore/app/models/author.rb
+++ Rails/bookstore/app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ApplicationRecord
-    validates :first_name, acceptance: true
-    validates :last_name, acceptance: true
-    validates :title, acceptance: true
+    validates :first_name, acceptance: true, length: {maximum: 10}
+    validates :last_name, acceptance: true, length: {maximum: 10}
+    validates :title, acceptance: true, length: {in: 5..20}
     #validates :terms_of_service, acceptance: true
 end
