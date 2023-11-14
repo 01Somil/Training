02aafaf89c1fa349ed12c9c84961736429039af1
01Somil <somilverma11@gmail.com>
2023-11-14 10:54:41 +0530

    Adding allow nil validation to suppliers table

--- Rails/bookstore/app/models/supplier.rb
+++ Rails/bookstore/app/models/supplier.rb
@@ -1,4 +1,4 @@
 class Supplier < ApplicationRecord
-    validates :name, format: {with: /\A[a-zA-Z]+\z/, message: "only allows letters"}
+    validates :name, format: {with: /\A[a-zA-Z]+\z/, message: "only allows letters"}, allow_nil: false
     validates :name, inclusion: {in: %w(John Paul David), message: "%{value} is not a valid name"}
 end
