2bffc662c9e34cd5eeeec7d890763210c633ac00
01Somil <somilverma11@gmail.com>
2023-11-14 11:27:40 +0530

    Adding on validation to first name in customers table

--- Rails/bookstore/app/models/customer.rb
+++ Rails/bookstore/app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-    validates :first_name, length: {maximum: 20}, allow_blank: true
+    validates :first_name, length: {maximum: 20}, allow_blank: false, on: :create
     validates :email, uniqueness: true, confirmation: {case_sensitive: false}
     validates :email_confirmation, presence: true
     validates :title, absence: true
