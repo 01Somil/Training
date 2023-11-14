39c4181d3a61eea16157099ac0a4bcb7427b5417
01Somil <somilverma11@gmail.com>
2023-11-14 11:19:54 +0530

    Adding allow_blank validation to customers table

--- Rails/bookstore/app/models/customer.rb
+++ Rails/bookstore/app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
+    validates :first_name, length: {maximum: 20}, allow_blank: true
     validates :email, uniqueness: true, confirmation: {case_sensitive: false}
     validates :email_confirmation, presence: true
     validates :title, absence: true
