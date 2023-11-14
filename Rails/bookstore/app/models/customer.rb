c030c9d4ef28fcbdee303af8e4e0f024a43c9590
01Somil <somilverma11@gmail.com>
2023-11-14 12:03:58 +0530

    Strict validations to customers table

--- Rails/bookstore/app/models/customer.rb
+++ Rails/bookstore/app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ApplicationRecord
     validates :first_name, length: {maximum: 20}, allow_blank: false, on: :create
     validates :email, uniqueness: true, confirmation: {case_sensitive: false}
-    validates :email_confirmation, presence: true
+    validates :email_confirmation, presence: {strict: true}
     validates :title, absence: true
 end
