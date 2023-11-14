15d0a10c7ebc18b746a5f6cc746024ef7d5a4519
01Somil <somilverma11@gmail.com>
2023-11-14 15:22:52 +0530

    Adding Custom Validation to customers table on first_name

--- Rails/bookstore/app/models/customer.rb
+++ Rails/bookstore/app/models/customer.rb
@@ -1,6 +1,15 @@
+class MyValidator<ActiveModel::Validator
+  def validate(record)
+    unless record.first_name.start_with?'X'
+      record.errors.add :first_name, "Provide a valid name!!"
+    end
+  end
+end
+
 class Customer < ApplicationRecord
-    validates :first_name, length: {maximum: 20}, allow_blank: false, on: :create
-    validates :email, uniqueness: true, confirmation: {case_sensitive: false}
-    validates :email_confirmation, presence: {strict: true}
-    validates :title, absence: true
+  validates :first_name, length: { maximum: 20 }, allow_blank: false, on: :create
+  validates :email, uniqueness: true, confirmation: { case_sensitive: false }
+  validates :email_confirmation, presence: { strict: true }
+  validates :title, presence: true
+  validates_with MyValidator
 end
