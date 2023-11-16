3c8e4c50f94dc7fc543e037b7c7b0e49b4a970f1
01Somil <somilverma11@gmail.com>
2023-11-16 11:15:57 +0530

    after_initialize and after_find callbacks to authors table

--- Rails/bookstore/app/models/author.rb
+++ Rails/bookstore/app/models/author.rb
@@ -1,19 +1,31 @@
 class Author < ApplicationRecord
-    validates :first_name, acceptance: true, length: {maximum: 10}
-    validates :last_name, acceptance: true, length: {maximum: 10}
-    validates :title, acceptance: true, length: {in: 5..20}
-    validate :first_name_length, :last_name_length
 
-    def first_name_length
-        if first_name.present? && first_name.length<2
-            errors.add(:first_name,"First name cannot be less than 2")
-        end
-    end
+  # Callbacks
+  after_create -> {puts " New author has been created"}
 
-    def last_name_length
-        if last_name.present? && last_name.length<2
-            errors.add(:last_name,"Last name cannot be less than 2")
-        end
+  after_initialize do |author|
+    puts "You have initialized an object."
+  end
+
+  after_find do |author|
+    puts "You have found an object"
+  end
+
+  # validations
+  validates :first_name, presence: true, length: { maximum: 10 }
+  validates :last_name, presence: true, length: { maximum: 10 }
+  validates :title, presence: true, length: { in: 5..20 }
+  validate :first_name_length, :last_name_length
+
+  def first_name_length
+    if first_name.present? && first_name.length < 2
+      errors.add(:first_name, "First name cannot be less than 2")
     end
-    
+  end
+
+  def last_name_length
+    if last_name.present? && last_name.length < 2
+      errors.add(:last_name, "Last name cannot be less than 2")
+    end
+  end
 end
